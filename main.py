eadd2668542783d11678c33fc6b6c7a1b499a563
Revonix <Ryanvong15@gmail.com>
2023-05-10 00:26:45 -0700

    Able to retrieve text from JSON file. Now need to tokenize and stem each token

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from nltk.tokenize import RegexpTokenizer
+import json
+from bs4 import BeautifulSoup
 
 #regTokenizer = RegexpTokenizer(r'\w+')
 
@@ -19,12 +21,33 @@ class Posting:
 '''
 Code to parse JSON file and tokenize words alphanumerically, ignoring stopwords. Will also fix broken HTML
 '''
-def parseFile():
+def parseFile(filePath: str):
+    
+    # filePath is a path to a JSON object file. Get the URL and content from obj file.
+    with open(filePath, 'r') as file:
+        json_obj = json.load(file)
+
+        url = json_obj['url']
+
+        # Using beautifulsoup to parse HTML content
+        page_obj = BeautifulSoup(json_obj['content'], 'lxml')
+
+        # Tokenizing the text and storing in dictionary. Key (token) value (frequency)
+        #print(page_obj.get_text())
+
+def pageTokenize()
+    '''
+    Tokenizes the content retrieved from BeautifulSoup's get_text().
+    Returns a dictionary of the tokens as keys and frequency as values. 
+    This tokenizer also takes the *stems* from every token and stores it as
+    keys. 
+    '''
     pass
+
 
 
 def run():
-    pass
+    parseFile("../developer/DEV/aiclub_ics_uci_edu/8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json")
 
 if __name__ == '__main__':
     run()
