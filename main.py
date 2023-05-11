2cb049c72d4f638be5513848ddd551c50c7edf5f
Revonix <Ryanvong15@gmail.com>
2023-05-10 23:10:06 -0700

    Revised tokens

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
 import json
 from bs4 import BeautifulSoup
+import os
+import re
+
+ALPHANUMERIC_WORDS = re.compile('[a-zA-Z0-9]+')
 
 class Posting:
     def __init__(self, id, frequency):
@@ -32,7 +36,7 @@ def parseFile(filePath: str):
         page_obj = BeautifulSoup(json_obj['content'], 'lxml')
 
         # Tokenizing the text and storing in dictionary. Key (token) value (frequency)
-        print(pageTokenize(page_obj))
+        return (pageTokenize(page_obj), url)
 
 def pageTokenize(page: object):
     '''
@@ -43,13 +47,15 @@ def pageTokenize(page: object):
     '''
 
     # Tokenizing the page and storing tokens in a list
-    regTokenizer = RegexpTokenizer(r'\w+')
-    tokens = regTokenizer.tokenize(page.get_text())
+    #regTokenizer = RegexpTokenizer(r'\w+')
+    #tokens = regTokenizer.tokenize(page.get_text())
+
+    text = page.get_text()
 
     # Stemming each token and adding to a dictionary
     stemmer = PorterStemmer()
     stems = dict()
-    for token in tokens:
+    for token in re.findall(ALPHANUMERIC_WORDS, text):
         stemmedWord = stemmer.stem(token)
 
         # Checking if it's already in the dictioanry - if it is, add by 1. If not, add a new entry
@@ -62,7 +68,62 @@ def pageTokenize(page: object):
 
 
 def run():
-    parseFile("../developer/DEV/aiclub_ics_uci_edu/8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json")
+    # Data structure (dictionary) to hold the inverted index in memory
+    index = dict()
+
+    # Dictionary to hold the mapping between page ID and url
+    pageIDs = dict()
+
+    pageCounter = 0
+
+    # Iterating through all the inner folders in DEV folder
+    path_to_inner = '../developer/DEV/'
+    for folder in os.listdir(path_to_inner):
+        
+        # Iterating through all the JSON files in the inner folder
+        json_files = []
+
+        for filename in os.listdir(os.path.join(path_to_inner, folder)):
+            json_files.append(os.path.join(path_to_inner, folder, filename))
+
+        for json_file in json_files:
+
+            # Processing each json file
+            words, url = parseFile(json_file)
+
+            for word, counter in words.items():
+
+                # Creating a posting
+                post = Posting(pageCounter, counter)
+
+                # Assigning to dictionary
+                if word in index:
+                    index[word].append(post)
+                else:
+                    index[word] = []
+                    index[word].append(post)
+
+            # After processing, store a mapping between the actual file and the id
+            pageIDs[pageCounter] = url
+            pageCounter += 1
+
+    # Once finished, put output to file
+    with open('results.txt', 'w', encoding='utf-8') as f:
+        f.write(f"Number of indexed documents: {len(pageIDs)}\n")
+        f.write(f"Number of tokens: {len(index)}\n")
+        f.write(f"Inverted Index\n")
+
+        for key, value in index.items():
+            f.write("\n------------------------------------------------\n")
+            f.write(f"Token: {key} - Documents: ")
+            for post in value:
+                f.write(f"{post.id}:{post.frequency} ")
+
+        f.write(f"ID Mappings")
+        for key, value in pageIDs.items():
+            f.write(f"{key} {value}\n")
+
+    #parseFile("../developer/DEV/aiclub_ics_uci_edu/8ef6d99d9f9264fc84514cdd2e680d35843785310331e1db4bbd06dd2b8eda9b.json")
 
 if __name__ == '__main__':
     run()
