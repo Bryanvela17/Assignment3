9bfac26f304549a578e4c9ad1040a3c5937f9039
Brandon Vela <bovela@uci.edu>
2023-05-18 20:39:59 -0700

    pushing a version of the engine file

--- main.py
+++ main.py
@@ -1,61 +1,128 @@
-import os
-import json
-import re
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
-from collections import defaultdict
+import json
 from bs4 import BeautifulSoup
+import os
+import re
 
 ALPHANUMERIC_WORDS = re.compile('[a-zA-Z0-9]+')
-wordmap = {}
+
+class Posting:
+    def __init__(self, id, frequency):
+        self._id = id
+        self._frequency = frequency
 
 
-def parse_single_file(filename):
-    # Read in the JSON data from the file
-    json_data = json.load(filename)
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def frequency(self):
+        return self._frequency
 
-    # Use beautifulsoup4 to remove all HTML tags
-    soup = BeautifulSoup(json.dumps(json_data), 'html.parser')
-    cleaned_json_data = soup.get_text()
-    cleaned_json_data = soup.strip().split()
-    print(cleaned_json_data)
+'''
+Code to parse JSON file and tokenize words alphanumerically, ignoring stopwords. Will also fix broken HTML
+'''
+def parseFile(filePath: str):
+    
+    # filePath is a path to a JSON object file. Get the URL and content from obj file.
+    with open(filePath, 'r') as file:
+        json_obj = json.load(file)
 
-def parse_json_folder(folder_path):
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.json'):
-                with open(os.path.join(root, file), 'r') as f:
-                    parse_single_file(f)
+        url = json_obj['url']
 
+        # Using beautifulsoup to parse HTML content
+        page_obj = BeautifulSoup(json_obj['content'], 'lxml')
 
-def get_alphanumeric_tokens(json_data):
-    alphanumeric_tokens = []
+        # Tokenizing the text and storing in dictionary. Key (token) value (frequency)
+        return (pageTokenize(page_obj), url)
+
+def pageTokenize(page: object):
+    '''
+    Tokenizes the content retrieved from BeautifulSoup's get_text().
+    Returns a dictionary of the tokens as keys and frequency as values. 
+    This tokenizer also takes the *stems* from every token and stores it as
+    keys. 
+    '''
+
+    # Tokenizing the page and storing tokens in a list
+    #regTokenizer = RegexpTokenizer(r'\w+')
+    #tokens = regTokenizer.tokenize(page.get_text())
+
+    text = page.get_text()
+
+    # Stemming each token and adding to a dictionary
     stemmer = PorterStemmer()
-    for data in json_data:
-        for value in data.values():
-            # Remove all non-alphanumeric characters from the value
-            value = re.sub(r'\W+', ' ', value)
-            # Split the value into tokens and filter out non-alphanumeric tokens
-            tokens = [token for token in value.split() if token.isalnum()]
-            # Apply Porter stemming to each token
-            stemmed_tokens = [stemmer.stem(token) for token in tokens]
-            alphanumeric_tokens.extend(stemmed_tokens)
-    return alphanumeric_tokens
+    stems = dict()
+    for token in re.findall(ALPHANUMERIC_WORDS, text):
+        stemmedWord = stemmer.stem(token)
+
+        # Checking if it's already in the dictioanry - if it is, add by 1. If not, add a new entry
+        if stemmedWord in stems:
+            stems[stemmedWord] += 1
+        else:
+            stems[stemmedWord] = 1
+
+    return stems
 
 
-def count_token_frequency(tokens):
-    frequency_dict = defaultdict(int)
-    for token in tokens:
-        frequency_dict[token] += 1
-    return frequency_dict
+def run():
+    # Data structure (dictionary) to hold the inverted index in memory
+    index = dict()
 
+    # Dictionary to hold the mapping between page ID and url
+    pageIDs = dict()
 
+    pageCounter = 0
+
+    # Iterating through all the inner folders in DEV folder
+    path_to_inner = '../developer/DEV/'
+    for folder in os.listdir(path_to_inner):
+        
+        # Iterating through all the JSON files in the inner folder
+        json_files = []
+
+        for filename in os.listdir(os.path.join(path_to_inner, folder)):
+            json_files.append(os.path.join(path_to_inner, folder, filename))
+
+        for json_file in json_files:
+
+            # Processing each json file
+            words, url = parseFile(json_file)
+
+            for word, counter in words.items():
+
+                # Creating a posting
+                post = Posting(pageCounter, counter)
+
+                # Assigning to dictionary
+                if word in index:
+                    index[word].append(post)
+                else:
+                    index[word] = []
+                    index[word].append(post)
+
+            # After processing, store a mapping between the actual file and the id
+            pageIDs[pageCounter] = url
+            pageCounter += 1
+
+    # Once finished, put output to file
+    with open('results.txt', 'w', encoding='utf-8') as f:
+        f.write(f"Number of indexed documents: {len(pageIDs)}\n")
+        f.write(f"Number of tokens: {len(index)}\n")
+        f.write(f"Inverted Index\n")
+
+        for key, value in index.items():
+            f.write("\n------------------------------------------------\n")
+            f.write(f"Token: {key} - Documents: ")
+            for post in value:
+                f.write(f"{post.id}:{post.frequency} ")
+
+        f.write(f"ID Mappings")
+        for key, value in pageIDs.items():
+            f.write(f"{key} {value}\n")
+
+            
 if __name__ == '__main__':
-    parse_json_folder('/Users/brandonvela/ICS121_InfoRetrieval/Milestone1/DEV/aiclub_ics_uci_edu')
-    alphanumeric_tokens = get_alphanumeric_tokens(json_data)
-    frequency_dict = count_token_frequency(alphanumeric_tokens)
-    # Sort the tokens by frequency count, in descending order
-    sorted_tokens = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
-
-    print("Token frequency (sorted):")
-    for token, count in sorted_tokens:
-        print(f"{token}: {count}")
+    run()
